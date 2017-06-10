23febff8f9e18504e117cf27bf8a8983ddeb19ad
Chris Moorhead <creativebrewdev@gmail.com>
2017-06-10 14:22:58 -0500

    AJAX (add todo & done/not)

--- client/script.js
+++ client/script.js
@@ -1,7 +1,56 @@
 var $ = require('jquery');
+var todoTemplate = require("../views/partials/todo.hbs");
+
+var addTodo = function() {
+   var text = $('#add-todo-text').val();
+   $.ajax({
+     url: '/api/todos',
+     type: 'POST',
+     data: {
+       text: text
+     },
+     dataType: 'json',
+     success: function(data) {
+       var todo = data.todo[0];
+       var newLiHtml = todoTemplate(todo);
+       $('form + ul').append(newLiHtml);
+       $('#add-todo-text').val('');
+     }
+   });
+ };
+
+var updateTodo = function(id, data, cb) {
+  $.ajax({
+    url: '/api/todos/'+id,
+    type: 'POST',
+    data: data,
+    dataType: 'json',
+    success: function(data) {
+      cb();
+    }
+  });
+};
 
 $(function() {
-    $('input').on('click', function() {
-        $(this).parent().toggleClass('checked');
+    $(":button").on('click', addTodo);
+    $(":text").on('keypress', function(e) {
+        var key = e.keyCode;
+        if( key == 13 || key == 169) {
+            addTodo();
+            e.preventDefault();
+            e.stopPropagation();
+            return false;
+        }
     });
+    $('ul').on('change', 'li :checkbox', function() {
+    var $this = $(this);
+    var $input = $this[0];
+    var $li = $this.parent();
+    var id = $li.attr('id');
+    var checked = $input.checked;
+    var data = { done: checked };
+    updateTodo(id, data, function(d) {
+      $this.next().toggleClass('checked');
+    });
+  });;
 });
--- models/todos.js
+++ models/todos.js
@@ -4,7 +4,7 @@ var Promise = require('bluebird');
 Promise.promisifyAll(mongoose);
 var TodoSchema = new Schema({
     text: {type: 'String', required: true},
-    done: {type: 'Boolean'}
+    done: {type: 'Boolean', required: true, default: false}
 });
 var Todo = mongoose.model('Todo', TodoSchema);
 module.exports = Todo;
